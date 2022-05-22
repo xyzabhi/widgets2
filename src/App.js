ac477e9cfe41ba4cc68b86db67d29a1f835228cc
Abhinav Kumar <abhinavkumar8083@gmail.com>
2022-05-23 00:39:24 +0530

    Working on the wikipedia search api .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -19,8 +20,7 @@ const items = [
 const App = () => {
   return (
     <div>
-      <br />
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
---
+++ src/components/Search.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const Search = () => {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+  console.log(results);
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    search();
+  }, [term]);
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input className="input" onChange={(e) => setTerm(e.target.value)} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
