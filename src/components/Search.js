0f3bb70f5b9b53ec5e6c444f255ef3ad63f1771a
Abhinav Kumar <abhinavkumar8083@gmail.com>
2022-05-23 18:41:58 +0530

    Wikipedia search widget completed useEffect+ Throttling Added.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from "react";
 const Search = () => {
   const [term, setTerm] = useState("programming");
   const [results, setResults] = useState([]);
-  console.log(results);
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -18,16 +17,48 @@ const Search = () => {
       });
       setResults(data.query.search);
     };
-    search();
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeOutId = setTimeout(() => {
+        if (term) search();
+      }, 500);
+      return () => {
+        clearTimeout(timeOutId);
+      };
+    }
   }, [term]);
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="content">
+          <div className="header">
+            <a
+              className="link"
+              href={`https://en.wikipedia.org?curid=${result.pageid}`}
+              target="_blank"
+            >
+              {result.title}
+            </a>
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
-          <input className="input" onChange={(e) => setTerm(e.target.value)} />
+          <input
+            className="input"
+            onChange={(e) => setTerm(e.target.value)}
+            value={term}
+          />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
